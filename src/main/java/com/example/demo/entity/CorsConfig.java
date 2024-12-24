ee7ac6bdc303f6a99091600e5f722b80f6cd3aae
NOUTAILAA <notaila7@gmail.com>
2024-12-24 23:50:48 +0100

    +junit

--- src/main/java/com/example/demo/entity/CorsConfig.java
+++ src/main/java/com/example/demo/entity/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:54203", "http://localhost:3000") // Frontend React
+                        .allowedOrigins("http://localhost:54203", "http://localhost:3000", "http://localhost:3001") // Frontend React
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
